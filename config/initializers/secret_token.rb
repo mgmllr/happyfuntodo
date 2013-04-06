14d619507521de9646e303d789a51535863a9c02
Max Miller <maxwellgmiller@gmail.com>
2013-04-06 12:20:35 -0400

    New Rails app with Twitter Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happyfuntodo::Application.config.secret_token = 'ea2bd61cce6b4e519879b30d53a55a052006152fb26d7e724a49eb55323c9a7c7b8df7cde8c2d07c0ed53621f8dd4e7ebb1a4fa0041a8f22a7237f8d644f11e4'
