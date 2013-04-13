e42958caeeff4ebe95812a185536f791ea77ac34
Max Miller <maxwellgmiller@gmail.com>
2013-04-13 16:31:19 -0400

    removed some files

--- db/migrate/20130406190131_set_default_for_items.rb
+++
@@ -1,9 +0,0 @@
-class SetDefaultForItems < ActiveRecord::Migration
-  def up
-  	change_column :items, :completed, :boolean, default: false 
-  end
-
-  def down
-  	raise ActiveRecord::IrreversibleMigration, "Can't remove the default"
-  end
-end
